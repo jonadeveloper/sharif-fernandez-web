07e611f6e30b40c203ba393e6614ef25d8944d84
jonatan villalva <jvillalva.sistemas@gmail.com>
2025-04-02 14:32:20 -0300

    creando estructura de articulos y dandole estilos

--- web-sharif/src/pages/home/Home.jsx
+++ web-sharif/src/pages/home/Home.jsx
@@ -13,27 +13,53 @@ return (
         <h2 className="title-section">
             Noticias - Novedades
         </h2>
-        <article>
-            <h3>SHARIF - TOUR MEXICO 2025</h3>
-            {/* <img src="" alt="" /> */}
-            <p>
-                Mi gente bonita, volvemos a mi México amado para cantar de nuevo con ustedes. En esta ocasión van a ser tres conciertos únicos y especiales, porque me traigo a Rapsusklei y a Juaninacka 
-                como invitados a cada uno de ellos. <br /> <br />No les quiero adelantar muchas sorpresas, pero para ir abriendo boca les diré que 
-                el evento constara de tres shows independientes. <br /> <br /> 
-                En la sección de conciertos tienen la información y el cartel de cada ciudad. <br /> <br />
+        <div className="container-articles">
+            <article className='article'>
+                <h3 className='title-article'>SHARIF - TOUR MEXICO 2025</h3>
+                {/* <img src="" alt="" /> */}
+                <p className='text'>
+                    Mi gente bonita, volvemos a mi México amado para cantar de nuevo con ustedes. En esta ocasión van a ser tres conciertos únicos y especiales, porque me traigo a Rapsusklei y a Juaninacka 
+                    como invitados a cada uno de ellos. <br /> <br />No les quiero adelantar muchas sorpresas, pero para ir abriendo boca les diré que 
+                    el evento constara de tres shows independientes. <br /> <br /> 
+                    En la sección de conciertos tienen la información y el cartel de cada ciudad. <br /> <br />
+                    Les besamos.
+                </p>
+            </article>
+            <article className='article'>
+                <h3 className='title-article'>
+                RAFAEL LECHOWSKI & SHARIF <br /> «UN INSTANTE ETERNO» Tour
+                </h3>
+                {/* <img src="" alt="" /> */}
+                <p className='text'>
+                Familia, no les descubro nada si les hablo del amor y admiracion que siento hacia Rafael Lechowski. 
+                En un bello y magico eclipse hemos unido fuerzas para darle a nuestra gente de Latam el show que se merecen.
+                <br /> <br />
+                Sera a lo largo de 8 conciertos (quiza alguno mas), donde Rafael y yo nos haremos cada uno un show inividual 
+                en el que cantaremos temas de todos nuestros discos, desde los mas antiguos a los mas nuevos.
+                <br /> <br />
+                Habra merchan, sorpresas y buenos versos.
+                <br /> <br />
                 Les besamos.
-            </p>
-        </article>
-        <article>
-            <h3></h3>
-            {/* <img src="" alt="" /> */}
-            <p></p>
-        </article>
-        <article>
-            <h3></h3>
-            {/* <img src="" alt="" /> */}
-            <p></p>
-        </article>
+                </p>
+                <br /> <br />
+                <p className="important">
+                P.D: Solo habra 35 meet & greet por ciudad.
+                </p>
+            </article>
+            <article className='article'>
+                <h3 className='title-article'>
+                    TOUR DIAS Y ELECTRICIDADES
+                </h3>
+                {/* <img src="" alt="" /> */}
+                <p className='text'>
+                Volvemos a los escenarios como siempre, con un show como nunca. Volvemos con un nuevo disco bajo el brazo y 
+                con un nuevo formato de directo en el que hemos incluido las mejores canciones de mis discos anteriores y 
+                las de este nuevo trabajo. Les esperamos a todos para cantar juntos, y brindar y celebrar bajo su cielo.
+                <br /> <br />
+                Les beso y les beso.
+                </p>
+            </article>
+        </div>
     </section>
     </>
 )
