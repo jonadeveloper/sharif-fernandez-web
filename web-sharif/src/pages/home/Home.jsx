5b4b6e25172ce38a9326af40a05f35686bb775e2
jonatan villalva <jvillalva.sistemas@gmail.com>
2025-05-21 00:56:04 -0300

    añadiendo seccion a home en donde se renderize el ultimo lanzamiento del artista

--- web-sharif/src/pages/home/Home.jsx
+++ web-sharif/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css';
 import { GiCompactDisc } from "react-icons/gi";
 import Modal from '../../components/modal/Modal';
 import Capricornio from '../../assets/capricornio-sf.webp';
+import CdCapricornio from '../../assets/CdCapricornio.webp';
 import { FaYoutube } from "react-icons/fa";
 import { FaSpotify } from "react-icons/fa";
 import { SiApplemusic } from "react-icons/si";
@@ -15,35 +16,35 @@ export default function Home() {
 return (
     <>
     <Slider>
-    <div className="slider description">
-        <h1>Sharif Fernandez</h1>
-            
-        <button className='cta' onClick={() => setIsOpen(true)}>
-                Escucha mi ultimo Disco - Capricornio 
-                <GiCompactDisc className='cd' />
-        </button>
-        <Modal className="modalDisc" open={isOpen} onClose={() => setIsOpen(false)}>
-                <h2 className='title-modal'>
-                    « Haz presave de capricornio aqui »          
-                </h2>
-                <img className='img-disc' src={Capricornio} alt="disco capricornio" />
-                <div className="listen-to-the-record">
-                    <a href="https://www.youtube.com/watch?v=0VYNu7_zlE4&list=PL1pbt-Jy-buU25GklXRR9NdXXk3uzJFXq" target='_blank'>
-                        <FaYoutube className='youtube' />
-                    </a>
-                    
-                    <a href="https://open.spotify.com/intl-es/album/7jKJG9mIRc0BoATP9paXhz" target='_blank'>
-                        <FaSpotify className='spotify' />
-                    </a>
-                    <a href="https://music.apple.com/ar/album/capricornio/1774030399" target='_blank'>
-                        <SiApplemusic className='headphone' />
-                    </a>
-                </div>
-        </Modal>
-    </div>
+        <div className="slider description">
+            <h1>Sharif Fernandez</h1>
+                
+            <button className='cta' onClick={() => setIsOpen(true)}>
+                    Escucha mi ultimo Disco - Capricornio 
+                    <GiCompactDisc className='cd' />
+            </button>
+            <Modal className="modalDisc" open={isOpen} onClose={() => setIsOpen(false)}>
+                    <h2 className='title-modal'>
+                        « Haz presave de capricornio aqui »          
+                    </h2>
+                    <img className='img-disc' src={Capricornio} alt="disco capricornio" />
+                    <div className="listen-to-the-record">
+                        <a href="https://www.youtube.com/watch?v=0VYNu7_zlE4&list=PL1pbt-Jy-buU25GklXRR9NdXXk3uzJFXq" target='_blank'>
+                            <FaYoutube className='youtube' />
+                        </a>
+                        
+                        <a href="https://open.spotify.com/intl-es/album/7jKJG9mIRc0BoATP9paXhz" target='_blank'>
+                            <FaSpotify className='spotify' />
+                        </a>
+                        <a href="https://music.apple.com/ar/album/capricornio/1774030399" target='_blank'>
+                            <SiApplemusic className='headphone' />
+                        </a>
+                    </div>
+            </Modal>
+        </div>
     </Slider>
     <section id="news">
-        <h2 className='section-news-title'>Ultimas noticias</h2>
+        <h2 className='section-title'>Ultimas noticias</h2>
         <div className='container-cards'>
             {articles.map(art => (
                 <NewsCard
@@ -53,6 +54,12 @@ return (
                 title={art.title}
                 />
             ))}
+        </div>
+    </section>
+    <section className="latest-release">
+        <h2 className='section-title'>Sharif - Capricornio</h2>
+        <div className="cd-sharif">
+            <img src={CdCapricornio} alt="capricornio cd" />
         </div>
     </section>
     </>
